1acbf290159f41b6a1016d681c279e38e16d2ece
Moon <alperozkan21@gmail.com>
2020-04-15 23:18:50 +0300

    Testing Github Connection

--- GKBot.py
+++ GKBot.py
@@ -1,8 +1,21 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!!')
+TOKEN = 'NzAwMDUyODAzMzg4NzAyODUz.XpdgmA.R2Qrsm8do47i2anxb3pxz6OQOxI'
 
+BOTPERMISSION = 133184
 
-a = 5;
-a
+class BotClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}'.format(self.user))
+    async def on_disconnect(self):
+        print('Disconnecting!')
+    async def on_message(self, message):
+        if(message.author == self.user):
+            return
+        if(message.content == 'ping'):
+            msg = 'Ping Received by {0.author.mention}'.format(message)
+            await message.channel.send(msg)
+
+client = BotClient()
+client.run(TOKEN)
